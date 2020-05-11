5de2f429dedcf3974019d57505e1cf28fac99139
Darshan <darshan9072@gmail.com>
2020-05-11 12:46:01 +0530

    This is first commit

---
+++ src/test/java/darshan/mycompany/app/AppiumTest.java
@@ -0,0 +1,16 @@
+package darshan.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/darshan/mycompany/app/SeleniumTest.java
@@ -0,0 +1,18 @@
+package darshan.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserautomation()
+	{
+		System.out.println("browserautomation");
+	}
+	@Test
+	public void elementuis()
+	{
+		System.out.println("elementuis");
+	}
+
+}

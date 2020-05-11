b1a8c3b1447f026f566e5ee310504927d369c664
Darshan <darshan9072@gmail.com>
2020-05-11 14:26:31 +0530

    Architect asian development

--- src/test/java/darshan/mycompany/app/AppiumTest.java
+++ src/test/java/darshan/mycompany/app/AppiumTest.java
@@ -12,5 +12,10 @@ public class AppiumTest {
 	public void IOSApps()
 	{
 		System.out.println("IOSApps");
+		System.out.println("IOSApps");
+		System.out.println("IOSApps");
+		System.out.println("IOSApps");
+		System.out.println("IOSApps");
+		System.out.println("IOSApps");
 	}
 }
